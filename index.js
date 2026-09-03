c8336b97557641217425ae85f2a1ef0dd7b3a833
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-09-03 15:21:42 +0530

    Add create command handling

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ const client = new Client({ intents:
      GatewayIntentBits.MessageContent]  
     });
 
-
 client.on('messageCreate', (message) => {
     if(message.author.bot) return;
+    if(message.content.startsWith('create')){
+        const url = message.content.split('create')[1]
+        return message.reply({
+            content: 'Generating Short ID for ' + url,
+        })
+    }
    message.reply({
     content: "Hi from Bot",
    })
+})
+
+client.on('interactionCreate', interaction => {
+    interaction.reply('Pong!!');
 })
 
 client.login(process.env.DISCORD_TOKEN);
